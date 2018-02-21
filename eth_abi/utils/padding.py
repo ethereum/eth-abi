00c08587c27e013a245f2d4cf164fcac5652e803
Piper Merriam <pipermerriam@gmail.com>
2018-02-21 09:52:25 -0700

    change how padding utils are written

--- eth_abi/utils/padding.py
+++ eth_abi/utils/padding.py
@@ -1,33 +1,27 @@
-def just(value, length, fillerchar, direction='left'):
-    if isinstance(value, str):
-        _fillerchar = fillerchar.decode()
-    elif isinstance(value, bytes):
-        _fillerchar = fillerchar
-    if direction == 'left':
-        return value.rjust(length, _fillerchar)
-    if direction == 'right':
-            return value.ljust(length, _fillerchar)
+from cytoolz import (
+    curry,
+)
 
 
+@curry
 def zpad(value, length):
-    return just(value, length, b'\x00')
+    return value.rjust(length, b'\x00')
 
 
-def zpad32(value):
-    return just(value, 32, b'\x00')
+zpad32 = zpad(length=32)
 
 
+@curry
 def zpad_right(value, length):
-    return just(value, length, b'\x00', direction='right')
+    return value.ljust(length, b'\x00')
 
 
-def zpad32_right(value):
-    return just(value, 32, b'\x00', direction='right')
+zpad32_right = zpad_right(length=32)
 
 
+@curry
 def fpad(value, length):
-    return just(value, length, b'\xff')
+    return value.rjust(length, b'\xff')
 
 
-def fpad32(value):
-    return just(value, 32, b'\xff')
+fpad32 = fpad(length=32)
