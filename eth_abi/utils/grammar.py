2c0964c7c291c762a26cfb4e43a5eea75a088da1
David Sanders <davesque@gmail.com>
2018-04-09 00:41:09 -0600

    Add parsimonious parser

---
+++ eth_abi/utils/grammar.py
@@ -0,0 +1,344 @@
+import functools
+import re
+
+from parsimonious import expressions
+import parsimonious
+
+from eth_abi.exceptions import (
+    ABITypeError,
+    ParseError,
+)
+
+
+grammar = parsimonious.Grammar(r"""
+type = tuple_type / basic_type
+
+tuple_type = "(" type next_type* ")"
+next_type = "," type
+
+basic_type = base sub? arrlist?
+
+base = alphas
+
+sub = two_size / digits
+two_size = (digits "x" digits)
+
+arrlist = (const_arr / dynam_arr)+
+const_arr = "[" digits "]"
+dynam_arr = "[]"
+
+alphas = ~"[a-z]+"
+digits = ~"[1-9][0-9]*"
+""")
+
+
+class NodeVisitor(parsimonious.NodeVisitor):
+    """
+    Parsimonious node visitor which performs both parsing of type strings and
+    post-processing of parse trees.  Parsing operations are cached.
+    """
+    grammar = grammar
+
+    def visit_tuple_type(self, node, visited_children):
+        # Ignore left and right parens
+        _, first, rest, _ = visited_children
+
+        return TupleType((first,) + rest, node=node)
+
+    def visit_next_type(self, node, visited_children):
+        # Ignore comma
+        _, abi_type = visited_children
+
+        return abi_type
+
+    def visit_basic_type(self, node, visited_children):
+        base, sub, arrlist = visited_children
+
+        return BasicType(base, sub, arrlist, node=node)
+
+    def visit_two_size(self, node, visited_children):
+        # Ignore "x"
+        first, _, second = visited_children
+
+        return first, second
+
+    def visit_const_arr(self, node, visited_children):
+        # Ignore left and right brackets
+        _, int_value, _ = visited_children
+
+        return (int_value,)
+
+    def visit_dynam_arr(self, node, visited_children):
+        return tuple()
+
+    def visit_alphas(self, node, visited_children):
+        return node.text
+
+    def visit_digits(self, node, visited_children):
+        return int(node.text)
+
+    def generic_visit(self, node, visited_children):
+        if isinstance(node.expr, expressions.OneOf):
+            # Unwrap value chosen from alternatives
+            return visited_children[0]
+
+        if isinstance(node.expr, expressions.Optional):
+            # Unwrap optional value or return `None`
+            if len(visited_children) != 0:
+                return visited_children[0]
+
+            return None
+
+        return tuple(visited_children)
+
+    @functools.lru_cache(maxsize=None)
+    def parse(self, type_str):
+        """
+        Caches and returns results of parsing operations.  Wraps any raised
+        parsing errors in a custom error class.
+        """
+        if not isinstance(type_str, str):
+            raise TypeError('Can only parse string values: got {}'.format(type(type_str)))
+
+        try:
+            return super().parse(type_str)
+        except parsimonious.ParseError as e:
+            raise ParseError(e.text, e.pos, e.expr)
+
+
+visitor = NodeVisitor()
+
+
+class ABIType:
+    """
+    Base class for classes which represent the results of parsing operations on
+    abi type strings after post-processing.
+    """
+    __slots__ = ('node',)
+
+    def __init__(self, *, node=None):
+        # The parsimonious `Node` instance associated with this parsed type may
+        # be optionally included.  If a type must be validated during a parsing
+        # operation, the `Node` instance is required since the `invalidate`
+        # method expects it.
+        self.node = node
+
+    def __repr__(self):  # pragma: no cover
+        return '<{} {}>'.format(type(self).__qualname__, repr(str(self)))
+
+    def __eq__(self, other):
+        """
+        Two ABI types are equal if their canonical string representations are
+        equal.
+        """
+        return (
+            type(self) is type(other) and
+            str(self) == str(other)
+        )
+
+    def __str__(self):  # pragma: no cover
+        """
+        An ABI type must have a canonical string representation.
+        """
+        raise NotImplementedError('Must implement `__str__`')
+
+    def validate(self):  # pragma: no cover
+        """
+        An ABI type must be able to validate itself against the solidity ABI
+        spec:
+        https://solidity.readthedocs.io/en/develop/abi-spec.html
+        """
+        raise NotImplementedError('Must implement `validate`')
+
+    def invalidate(self, error_msg):
+        """
+        Invalidates an ABI type with the given error message.  Expects that a
+        parsimonious node was provided from the original parsing operation that
+        yielded this type.
+        """
+        node = self.node
+
+        raise ABITypeError(
+            "For '{comp_str}' type at column {col} "
+            "in '{type_str}': {error_msg}".format(
+                comp_str=node.text,
+                col=node.start + 1,
+                type_str=node.full_text,
+                error_msg=error_msg,
+            ),
+        )
+
+
+class TupleType(ABIType):
+    """
+    Represents the result of parsing a type string which contains a tuple abi
+    type.
+
+    e.g. "(int,bool)"
+    """
+    __slots__ = ('components',)
+
+    def __init__(self, components, *, node=None):
+        super().__init__(node=node)
+
+        self.components = components
+
+    def __str__(self):
+        return '({})'.format(','.join(str(c) for c in self.components))
+
+    def validate(self):
+        # A tuple type is valid if all of its components are valid i.e. if none
+        # of its components contain an invalid type such as "uint7"
+        for c in self.components:
+            c.validate()
+
+
+class BasicType(ABIType):
+    """
+    Represents the result of parsing a type string which contains a basic abi
+    type.
+
+    e.g. "uint", "address", "ufixed128x19[][2]"
+    """
+    __slots__ = ('base', 'sub', 'arrlist')
+
+    def __init__(self, base, sub=None, arrlist=None, *, node=None):
+        super().__init__(node=node)
+
+        self.base = base
+        self.sub = sub
+        self.arrlist = arrlist
+
+    def __str__(self):
+        sub, arrlist = self.sub, self.arrlist
+
+        if isinstance(sub, int):
+            sub = str(sub)
+        elif isinstance(sub, tuple):
+            sub = 'x'.join(str(s) for s in sub)
+        else:
+            sub = ''
+
+        if isinstance(arrlist, tuple):
+            arrlist = ''.join(repr(list(a)) for a in arrlist)
+        else:
+            arrlist = ''
+
+        return self.base + sub + arrlist
+
+    @property
+    def item_type(self):
+        """
+        If this type is an array type, returns the type of the array's items.
+        """
+        if self.arrlist is None:
+            raise ValueError(
+                "Cannot determine item type for non-array type '{}'".format(self)
+            )
+
+        return type(self)(
+            self.base,
+            self.sub,
+            self.arrlist[:-1] or None,
+            node=self.node,
+        )
+
+    def validate(self):
+        """
+        A basic type is valid if it appears to be one of the default types
+        described in the solidity ABI spec and its components don't violate any
+        of the assumptions set forth in that spec -or- if it does not appear to
+        be a default type.
+
+        Details found here:
+        https://solidity.readthedocs.io/en/develop/abi-spec.html
+        """
+        base, sub = self.base, self.sub
+
+        # Check validity of string type
+        if base in ('string', 'bytes'):
+            if not (sub is None or isinstance(sub, int)):
+                self.invalidate('string type must have either no suffix or a numerical suffix')
+
+            if isinstance(sub, int) and sub > 32:
+                self.invalidate('maximum 32 bytes for fixed-length string or bytes')
+
+        # Check validity of integer type
+        elif base in ('int', 'uint'):
+            if not isinstance(sub, int):
+                self.invalidate('integer type must have numerical suffix')
+
+            if sub < 8 or 256 < sub:
+                self.invalidate('integer size out of bounds (max 256 bits)')
+
+            if sub % 8 != 0:
+                self.invalidate('integer size must be multiple of 8')
+
+        # Check validity of fixed type
+        elif base in ('fixed', 'ufixed'):
+            if not isinstance(sub, tuple):
+                self.invalidate(
+                    'fixed type must have suffix of form <bits>x<exponent>, e.g. 128x19',
+                )
+
+            bits, minus_e = sub
+
+            if bits < 8 or 256 < bits:
+                self.invalidate('fixed size out of bounds (max 256 bits)')
+
+            if bits % 8 != 0:
+                self.invalidate('fixed size must be multiple of 8')
+
+            if minus_e < 1 or 80 < minus_e:
+                self.invalidate(
+                    'fixed exponent size out of bounds, {} must be in 1-80'.format(
+                        minus_e,
+                    ),
+                )
+
+        # Check validity of real type
+        elif base in ('real', 'ureal'):
+            if not isinstance(sub, tuple):
+                self.invalidate('real type must have suffix of form <high>x<low>, e.g. 128x128')
+
+            high, low = sub
+
+            if (high + low) < 8 or 256 < (high + low):
+                self.invalidate('real size out of bounds (max 256 bits)')
+
+            if high % 8 != 0 or low % 8 != 0:
+                self.invalidate('real high/low sizes must be multiples of 8')
+
+        # Check validity of hash type
+        elif base == 'hash':
+            if not isinstance(sub, int):
+                self.invalidate('hash type must have numerical suffix')
+
+        # Check validity of address type
+        elif base == 'address':
+            if sub is not None:
+                self.invalidate('address cannot have suffix')
+
+
+TYPE_ALIASES = {
+    'int': 'int256',
+    'uint': 'uint256',
+    'fixed': 'fixed128x19',
+    'ufixed': 'ufixed128x19',
+    'function': 'bytes24',
+}
+
+TYPE_ALIAS_RE = re.compile(r'\b({})\b'.format(
+    '|'.join(re.escape(a) for a in TYPE_ALIASES.keys())
+))
+
+
+def normalize(type_str):
+    # Replace aliases with substitutions
+    return TYPE_ALIAS_RE.sub(
+        lambda match: TYPE_ALIASES[match.group(0)],
+        type_str,
+    )
+
+
+parse = visitor.parse
---
+++ tests/utility/test_grammar.py
@@ -0,0 +1,204 @@
+import pytest
+
+from hypothesis import (
+    example,
+    given,
+    strategies as st,
+)
+
+from eth_abi.exceptions import (
+    ABITypeError,
+    ParseError,
+)
+
+from eth_abi.utils.grammar import (
+    BasicType,
+    TYPE_ALIASES,
+    TupleType,
+    normalize,
+    parse,
+)
+
+from ..property.abi_type_strategies import (
+    malformed_type_strs,
+    type_strs,
+)
+
+
+@pytest.mark.parametrize(
+    'type_str, expected_type',
+    (
+        ('uint', BasicType('uint')),
+        ('uint256', BasicType('uint', 256)),
+        ('uint256[]', BasicType('uint', 256, ((),))),
+        ('uint256[][1][23][]', BasicType('uint', 256, ((), (1,), (23,), ()))),
+        ('uint[]', BasicType('uint', None, ((),))),
+        ('int[2]', BasicType('int', None, ((2,),))),
+        ('fixed128x19', BasicType('fixed', (128, 19))),
+        ('ufixed128x19[][]', BasicType('ufixed', (128, 19), ((), ()))),
+        (
+            '(int,ufixed128x19[][])',
+            TupleType((
+                BasicType('int'),
+                BasicType('ufixed', (128, 19), ((), ())),
+            )),
+        ),
+        (
+            '(int,(address,function))',
+            TupleType((
+                BasicType('int'),
+                TupleType((
+                    BasicType('address'),
+                    BasicType('function'),
+                )),
+            )),
+        ),
+        (
+            '((address,(fixed,bool)),(uint256[][1][],fixed128x19))',
+            TupleType((
+                TupleType((
+                    BasicType('address'),
+                    TupleType((
+                        BasicType('fixed'),
+                        BasicType('bool'),
+                    )),
+                )),
+                TupleType((
+                    BasicType('uint', 256, ((), (1,), ())),
+                    BasicType('fixed', (128, 19)),
+                )),
+            )),
+        ),
+    )
+)
+def test_parsing_with_parsimonious_grammar_and_node_visitor_works(type_str, expected_type):
+    assert parse(type_str) == expected_type
+
+
+@given(malformed_type_strs, st.none())
+@example('int0', 4)
+@example('fixed128x0', 9)
+@example('ufixed128x0', 10)
+@example('int!', 4)
+@example('(int, bool)', 6)
+@example('(int,bool,)', 11)
+@example('(int,(address,uint256[][])', 27)
+def test_parsing_invalid_type_str_causes_parse_error(type_str, error_col):
+    if error_col is not None:
+        pattern = r'Parse error at .* \(column {}\)'.format(error_col)
+    else:
+        pattern = r'Parse error at .*'
+
+    with pytest.raises(ParseError, match=pattern):
+        parse(type_str)
+
+
+def test_parse_raises_type_error_for_wrong_data_type():
+    with pytest.raises(TypeError):
+        parse(b'uint256')
+
+
+@given(type_strs)
+def test_end_to_end_parsing_and_collapsing(type_str):
+    assert str(parse(type_str)) == type_str
+
+
+@pytest.mark.parametrize(
+    'type_str, pattern',
+    (
+        ('string1x1', 'no suffix or a numerical'),
+        ('string33', 'maximum 32 bytes'),
+        ('bytes1x1', 'no suffix or a numerical'),
+        ('bytes33', 'maximum 32 bytes'),
+        ('int', 'must have numerical suffix'),
+        ('int7', 'size out of bounds'),
+        ('int257', 'size out of bounds'),
+        ('int9', 'must be multiple of 8'),
+        ('uint', 'must have numerical suffix'),
+        ('uint7', 'size out of bounds'),
+        ('uint257', 'size out of bounds'),
+        ('uint9', 'must be multiple of 8'),
+        ('fixed', 'must have suffix of form'),
+        ('fixed8', 'must have suffix of form'),
+        ('fixed7x19', 'fixed size out of bounds'),
+        ('fixed257x19', 'fixed size out of bounds'),
+        ('fixed17x19', 'must be multiple of 8'),
+        ('fixed128x81', 'exponent size out of bounds'),
+        ('ufixed', 'must have suffix of form'),
+        ('ufixed8', 'must have suffix of form'),
+        ('ufixed7x19', 'fixed size out of bounds'),
+        ('ufixed257x19', 'fixed size out of bounds'),
+        ('ufixed17x19', 'must be multiple of 8'),
+        ('ufixed128x81', 'exponent size out of bounds'),
+        ('real', 'must have suffix of form'),
+        ('real8', 'must have suffix of form'),
+        ('real6x1', 'real size out of bounds'),
+        ('real256x1', 'real size out of bounds'),
+        ('real17x8', 'must be multiples of 8'),
+        ('real16x9', 'must be multiples of 8'),
+        ('ureal', 'must have suffix of form'),
+        ('ureal8', 'must have suffix of form'),
+        ('ureal6x1', 'real size out of bounds'),
+        ('ureal256x1', 'real size out of bounds'),
+        ('ureal17x8', 'must be multiples of 8'),
+        ('ureal16x9', 'must be multiples of 8'),
+        ('hash', 'must have numerical suffix'),
+        ('hash128x128', 'must have numerical suffix'),
+        ('address128', 'cannot have suffix'),
+        ('address128x128', 'cannot have suffix'),
+        (
+            '(int256,(address,(ufixed7x19,fixed)))',
+            "For 'ufixed7x19' type at column 19 "
+            r"in '\(int256,\(address,\(ufixed7x19,fixed\)\)\)': "
+            "fixed size out of bounds",
+        ),
+        (
+            '(int256,(address,(ufixed128x19,fixed)))',
+            "For 'fixed' type at column 32 "
+            r"in '\(int256,\(address,\(ufixed128x19,fixed\)\)\)': "
+            "fixed type must have suffix of form",
+        ),
+    )
+)
+def test_invalid_abi_types(type_str, pattern):
+    with pytest.raises(ABITypeError, match=pattern):
+        parse(type_str).validate()
+
+
+@pytest.mark.parametrize(
+    'type_str',
+    (
+        'string',
+        'bytes24',
+        'int256',
+        'uint128',
+        'fixed128x19',
+        'ufixed256x19',
+        'real128x128',
+        'ureal128x64',
+        'hash256',
+        'address',
+        '(int256,(address,(ufixed128x19,fixed128x19)))',
+    )
+)
+def test_valid_abi_types(type_str):
+    parse(type_str).validate()
+
+
+@pytest.mark.parametrize(
+    'type_str, normalized',
+    tuple(TYPE_ALIASES.items()) + (
+        ('(int,uint,fixed,ufixed)', '(int256,uint256,fixed128x19,ufixed128x19)'),
+        ('(function,function,function)', '(bytes24,bytes24,bytes24)'),
+    ),
+)
+def test_normalize(type_str, normalized):
+    assert normalize(type_str) == normalized
+
+
+def test_basic_type_item_type_throws_errors():
+    bt = parse('int256')
+
+    pattern = "Cannot determine item type for non-array type 'int256'"
+    with pytest.raises(ValueError, match=pattern):
+        bt.item_type
