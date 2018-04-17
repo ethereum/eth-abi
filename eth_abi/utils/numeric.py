b5bfa1d5978ed26bd8f85535cadd79c8f8ee0469
David Sanders <davesque@gmail.com>
2018-04-16 19:44:33 -0600

    Remove python 2 code

--- eth_abi/utils/numeric.py
+++ eth_abi/utils/numeric.py
@@ -1,9 +1,7 @@
-import sys
 import math
 import decimal
 
 from eth_utils import (
-    encode_hex,
     is_number,
 )
 
@@ -12,31 +10,13 @@ from eth_abi.constants import (
 )
 
 
-if sys.version_info.major == 2:
-    import struct
+def int_to_big_endian(value):
+    byte_length = math.ceil(value.bit_length() / 8)
+    return value.to_bytes(byte_length, byteorder='big')
 
-    def int_to_big_endian(value):
-        cs = []
-        while value > 0:
-            cs.append(chr(value % 256))
-            value /= 256
-        s = ''.join(reversed(cs))
-        return s
 
-    def big_endian_to_int(value):
-        if len(value) == 1:
-            return ord(value)
-        elif len(value) <= 8:
-            return struct.unpack('>Q', value.rjust(8, '\x00'))[0]
-        else:
-            return int(encode_hex(value), 16)
-else:
-    def int_to_big_endian(value):
-        byte_length = math.ceil(value.bit_length() / 8)
-        return (value).to_bytes(byte_length, byteorder='big')
-
-    def big_endian_to_int(value):
-        return int.from_bytes(value, byteorder='big')
+def big_endian_to_int(value):
+    return int.from_bytes(value, byteorder='big')
 
 
 abi_decimal_context = decimal.Context(prec=999)
