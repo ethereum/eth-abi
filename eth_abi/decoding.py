52bc0081fefcb3fd0b05334b00e15ab62cd18a51
David Sanders <davesque@gmail.com>
2018-04-30 18:09:50 -0600

    Improve docstring

--- eth_abi/decoding.py
+++ eth_abi/decoding.py
@@ -29,12 +29,12 @@ from eth_abi.utils.numeric import (
 
 class ContextFramesBytesIO(io.BytesIO):
     """
-    A byte stream which can track a series of contextual frames in a stack.
-    This data structure is necessary to perform nested decodings using the
-    ``HeadTailDecoder`` since offsets present in head sections are relative
-    only to a particular encoded object.  These offsets can only be used to
-    locate a position in a decoding stream if they are paired with a contextual
-    offset that establishes the position of the object in which they are found.
+    A byte stream which can track a series of contextual frames in a stack. This
+    data structure is necessary to perform nested decodings using the
+    ``HeadTailDecoder`` since offsets present in head sections are relative only
+    to a particular encoded object.  These offsets can only be used to locate a
+    position in a decoding stream if they are paired with a contextual offset
+    that establishes the position of the object in which they are found.
 
     For example, consider the encoding of a value for the following type:
 
@@ -42,27 +42,30 @@ class ContextFramesBytesIO(io.BytesIO):
     value: (1,(2,[3,3]))
 
     There are two tuples in this type: one inner and one outer.  The inner tuple
-    type contains a dynamic type `int[]` and, therefore, is itself dynamic.  The
-    outer tuple is also a dynamic type since it contains the dynamic inner
-    tuple.  Encoded, the value `(1,(2,[3,3]))` of type `(int,(int,int[]))` would
-    look like this (the data values have been truncated to remove the redundant
+    type contains a dynamic type `int[]` and, therefore, is itself dynamic.
+    This means that its value encoding will be placed in the tail section of the
+    outer tuple's encoding.  Furthermore, the inner tuple's encoding will,
+    itself, contain a tail section with the encoding for `[3,3]`.  All together,
+    the encoded value of `(1,(2,[3,3]))` would look like this (the data values
+    are normally 32 bytes wide but have been truncated to remove the redundant
     zeros at the beginnings of their encodings):
 
-                       offset data
-    ------------------------------
-         ^                  0 0x01
-         |                 32 0x40 <-- Offset of object A in global frame (64)
-    -----|------------------------
-    Global frame   ^       64 0x02 <-- Beginning of object A (64 w/offset 0 = 64)
-         |         |       96 0x40 <-- Offset of object B in frame of object A (64)
-    -----|-Object A's frame-------
-         |         |      128 0x02 <-- Beginning of object B (64 w/offset 64 = 128)
-         |         |      160 0x03
-         v         v      192 0x03
-    ------------------------------
+                   offset data
+    --------------------------
+         ^              0 0x01
+         |             32 0x40 <-- Offset of object A in global frame (64)
+    -----|--------------------
+    Global frame ^     64 0x02 <-- Beginning of object A (64 w/offset 0 = 64)
+         |       |     96 0x40 <-- Offset of object B in frame of object A (64)
+    -----|-Object A's frame---
+         |       |    128 0x02 <-- Beginning of object B (64 w/offset 64 = 128)
+         |       |    160 0x03
+         v       v    192 0x03
+    --------------------------
 
-    Note that the offset of object B (64) is relative to the encoding of object
-    A.  However, globally, object B is located at offset 128.  In order to make
+    Note that the offset of object B is encoded as 64 which only specifies the
+    beginning of its encoded value relative to the beginning of object A's
+    encoding.  Globally, object B is located at offset 128.  In order to make
     sense out of object B's offset, it needs to be positioned in the context of
     its enclosing object's frame (object A).
     """
