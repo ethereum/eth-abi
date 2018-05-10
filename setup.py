4692b504b63d83504c332f07a270fd39fec55a8c
Jason Carver <ut96caarrs@snkmail.com>
2018-05-10 10:42:12 -0700

    Bump version: 1.1.0 → 1.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import (
 setup(
     name='eth-abi',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='1.1.0',
+    version='1.1.1',
     description="""Ethereum ABI Utils""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
