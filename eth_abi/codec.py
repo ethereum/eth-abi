d63273c3bd3a94a2fcfede0de05e7dd45533fead
Stefan Mendoza <stefan@stefanmendoza.com>
2018-11-24 11:48:15 -0600

    Added test for null registry to basecodec

--- eth_abi/codec.py
+++ eth_abi/codec.py
@@ -32,7 +32,7 @@ class BaseABICodecEncoder():
     def __init__(self, registry):
         if registry is None:
             raise ValueError("`registry` may not be None")
-        
+
         self._registry = registry
 
     def encode_single(self, typ: TypeStr, arg: Any) -> bytes:
