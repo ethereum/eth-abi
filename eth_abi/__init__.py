ad7ec2ba3f958684b065531ff5a26af9f9a5c562
David Sanders <davesque@gmail.com>
2019-03-19 16:02:11 -0600

    Fix isort issues

--- eth_abi/__init__.py
+++ eth_abi/__init__.py
@@ -1,12 +1,11 @@
 import pkg_resources
 
 from eth_abi.abi import (  # NOQA
-    decode_single,
     decode_abi,
-    encode_single,
+    decode_single,
     encode_abi,
+    encode_single,
     is_encodable,
 )
-
 
 __version__ = pkg_resources.get_distribution('eth-abi').version
