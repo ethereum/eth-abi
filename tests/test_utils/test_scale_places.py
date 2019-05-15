d44d8c62212944998b252c97cca29505e845494b
David Sanders <davesque@gmail.com>
2019-05-15 13:02:58 -0600

    Bit of cleanup and whitespace

--- tests/test_utils/test_scale_places.py
+++ tests/test_utils/test_scale_places.py
@@ -1,5 +1,10 @@
 import decimal
 import itertools
+from typing import (
+    Iterable,
+    Iterator,
+    TypeVar,
+)
 
 from hypothesis import (
     given,
@@ -12,7 +17,13 @@ from eth_abi.utils.numeric import (
 )
 
 
-def take(n, it):
+T = TypeVar('T')
+
+
+def take(n: int, it: Iterable[T]) -> Iterator[T]:
+    """
+    Returns an iterator that takes ``n`` items from the iterable ``it``.
+    """
     yield from (i for _, i in zip(range(n), it))
 
 
@@ -37,9 +48,7 @@ def get_test_value(prec: int, frac_places: int) -> decimal.Decimal:
         return decimal.Decimal(decimal_repr[:whole_places] + '.' + decimal_repr[-frac_places:])
 
 
-@given(
-    st.integers(min_value=1, max_value=ABI_DECIMAL_PREC),
-)
+@given(st.integers(min_value=1, max_value=ABI_DECIMAL_PREC))
 def test_scale_places_pos(size):
     scaled_by_string = get_test_value(size, size)
 
@@ -49,9 +58,7 @@ def test_scale_places_pos(size):
     assert scaled_by_string == scaled_by_math
 
 
-@given(
-    st.integers(min_value=1, max_value=ABI_DECIMAL_PREC),
-)
+@given(st.integers(min_value=1, max_value=ABI_DECIMAL_PREC))
 def test_scale_places_neg(size):
     scaled_by_string = get_test_value(size, -size)
 
