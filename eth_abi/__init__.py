0f6434872ed3b8028f04d452bf576be7c7ee6c85
Felipe Selmo <fselmo2@gmail.com>
2022-04-04 16:25:17 -0600

    Add deprecation warning for python 3.6 version

--- eth_abi/__init__.py
+++ eth_abi/__init__.py
@@ -1,3 +1,6 @@
+import sys
+import warnings
+
 import pkg_resources
 
 from eth_abi.abi import (  # NOQA
@@ -10,5 +13,13 @@ from eth_abi.abi import (  # NOQA
     is_encodable,
     is_encodable_type,
 )
+
+if sys.version_info[1] == 6:
+    # reference setup.py -> python_requires
+    warnings.warn(
+        "eth-abi version 3.0.0 will no longer support Python 3.6",
+        category=DeprecationWarning,
+        stacklevel=2
+    )
 
 __version__ = pkg_resources.get_distribution('eth-abi').version
