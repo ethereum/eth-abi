02a6cbd575a9c53f7a4e09b78a22ed70a2a87919
kclowes <kclowes@users.noreply.github.com>
2022-07-07 16:27:37 -0600

    Fix tests, remove one that was a duplicate test of logic

--- tests/utils/test_abbr.py
+++ tests/utils/test_abbr.py
@@ -6,16 +6,23 @@ from eth_abi.utils.string import (
 
 
 @pytest.mark.parametrize(
-    'value,expected,limit',
+    "value,expected,limit",
     (
-        (1234567891234567891, '1234567891234567891', None),
-        (12345678912345678912, '12345678912345678912', None),
-        (123456789123456789123, '12345678912345678...', None),
-        ('asdf' * 30, "'asdfasdfasdfasdf...", None),
-        (list(range(100)), '[0, 1, 2, 3, 4, 5...', None),
-        (1234567891234567891, '...', 3),
-        (1234567891234567891, '1...', 4),
-    )
+        (1234567891234567891, "1234567891234567891", None),
+        (12345678912345678912, "12345678912345678912", None),
+        (
+            "asdf" * 30,
+            "'asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasd...",
+            None,
+        ),
+        (
+            list(range(100)),
+            "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 2...",  # noqa: E501
+            None,
+        ),
+        (1234567891234567891, "...", 3),
+        (1234567891234567891, "1...", 4),
+    ),
 )
 def test_abbr(value, expected, limit):
     if limit is not None:
@@ -27,4 +34,4 @@ def test_abbr(value, expected, limit):
 
 def test_abbr_throws_value_errors():
     with pytest.raises(ValueError):
-        abbr('asdf', limit=2)
+        abbr("asdf", limit=2)
