7dec28b49608f7a4bd1b6469ac33f94c7c8f32e4
kclowes <kclowes@users.noreply.github.com>
2022-11-16 12:10:00 -0700

    Remove upper pins on eth-utils and eth-typing

--- setup.py
+++ setup.py
@@ -65,8 +65,8 @@ setup(
     url="https://github.com/ethereum/eth-abi",
     include_package_data=True,
     install_requires=[
-        "eth-utils>=2.0.0,<3.0.0",
-        "eth-typing>=3.0.0,<4.0.0",
+        "eth-utils>=2.0.0",
+        "eth-typing>=3.0.0",
         "parsimonious>=0.8.0,<0.10.0",
     ],
     python_requires=">=3.7, <4",
