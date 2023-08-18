a8d5de3abce311d59b7952a87511b1654641d9d0
fselmo <fselmo2@gmail.com>
2023-08-18 16:43:09 -0600

    Refactor for zero-sized tuple validation:
    - Keep param validation for ``encode()`` and ``decode()`` where they are but the type string validation didn't make a whole lot of sense there. There's a grammar.parse that takes place trying to identify type strings and we give a very general error message when they can't be parsed. Adding some logic here when we know what the parse error is seems like a better, more central spot to take care of better messaging around these types of errors.
    - Along the way, add a bit more generalized logic around ``is_encodable_type()`` and ``is_encodable()``. We don't need to sift through different error types when we are looking for a ``bool`` answer. These methods ideally shouldn't throw any errors and adding exception classes to it is just a chore. Catch any exception and return ``False``, otherwise, when there are no errors, return ``True``. The exception to this is when multiple entries are found, that seems like a good case to throw.

--- eth_abi/utils/validation.py
+++ eth_abi/utils/validation.py
@@ -1,11 +1,7 @@
 from typing import (
     Any,
-    Iterable,
 )
 
-from eth_typing import (
-    TypeStr,
-)
 from eth_utils import (
     is_bytes,
 )
@@ -24,16 +20,3 @@ def validate_list_like_param(param: Any, param_name: str) -> None:
             f"The `{param_name}` value type must be one of list or tuple. "
             f"Got {type(param)}"
         )
-
-
-def validate_against_zero_sized_tuples(types: Iterable[TypeStr]) -> None:
-    if any("()" in type_str for type_str in types):
-        raise ValueError('Zero-sized tuple types "()" are not supported.')
-
-
-def validate_codec_types(types: Iterable[TypeStr]) -> None:
-    """
-    Validate that the provided types are valid for encoding or decoding.
-    """
-    validate_list_like_param(types, "types")
-    validate_against_zero_sized_tuples(types)
