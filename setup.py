5becd62aa7181406cfba9224341de58396bcc90c
fselmo <fselmo2@gmail.com>
2023-08-28 12:48:41 -0600

    Bump version: 4.1.0 → 4.2.0

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ with open("./README.md") as readme:
 setup(
     name="eth_abi",
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version="4.1.0",
+    version="4.2.0",
     description="""eth_abi: Python utilities for working with Ethereum ABI definitions, especially encoding and decoding""",
     long_description=long_description,
     long_description_content_type="text/markdown",
