bd13393be75a18de1648787afd7270a070cc4382
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-08-09 15:13:12 -0400

    feat: support future pythons<4 (#11)

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ setup(
         "mypy_extensions",
         "parsimonious>=0.10.0,<0.11.0",
     ],
-    python_requires=">=3.8, <3.15",
+    python_requires=">=3.8, <4",
     extras_require=extras_require,
     py_modules=["eth_abi"],
     license="MIT",
